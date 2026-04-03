84a1cab8305a4d1d8c09b42ce24d20332945989d
ImRehan-28 <173938869+ImRehan-28@users.noreply.github.com>
2026-04-03 21:45:29 +0530

    Update config, Docker setup, and test profile

--- src/main/java/com/rehan/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/rehan/ecommerce/config/SecurityConfig.java
@@ -54,10 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173",                       // ✅ local
-                "https://your-frontend.azurestaticapps.net"    // 🔥 replace after deploy
-        ));
+        configuration.setAllowedOrigins(List.of("*"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
